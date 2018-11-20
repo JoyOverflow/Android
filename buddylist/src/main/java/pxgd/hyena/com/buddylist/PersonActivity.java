2903ed391a574d3017d7e3f7c81a58b0bd3c172c
joyoverflow <joyoverflow@sina.com>
2018-11-21 00:17:44 +0800

    master 18:15

--- buddylist/src/main/java/pxgd/hyena/com/buddylist/PersonActivity.java
+++ buddylist/src/main/java/pxgd/hyena/com/buddylist/PersonActivity.java
@@ -1,13 +1,44 @@
 package pxgd.hyena.com.buddylist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class PersonActivity extends AppCompatActivity {
+public class PersonActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+
+        Person[] list={
+                new Person(R.drawable.mary,"徐浩然",42,"",""),
+                new Person(R.drawable.mary,"王启帆",32,"",""),
+                new Person(R.drawable.mary,"田展旭",28,"",""),
+                new Person(R.drawable.mary,"何欣朋",35,"",""),
+                new Person(R.drawable.mary,"唐艺芯",36,"",""),
+                new Person(R.drawable.mary,"赵悦",29,"","")
+
+        };
+        PersonAdapter ada=new PersonAdapter(
+                this, android.R.layout.simple_list_item_activated_2, list
+        );
+        setListAdapter(ada);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Person p=(Person)l.getItemAtPosition(position);
+        Intent i=new Intent(this,ProfileActivity.class);
+        i.putExtra(ProfileActivity.Image,p.image);
+        i.putExtra(ProfileActivity.Name,p.name);
+        i.putExtra(ProfileActivity.Age,p.age);
+        i.putExtra(ProfileActivity.Location,p.location);
+        i.putExtra(ProfileActivity.Descr,p.descr);
+        startActivity(i);
     }
 }
---
+++ buddylist/src/main/java/pxgd/hyena/com/buddylist/ProfileActivity.java
@@ -0,0 +1,58 @@
+package pxgd.hyena.com.buddylist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView name;
+    private TextView location;
+    private TextView age;
+    private TextView onlineStatus;
+    private EditText description;
+    private ImageView profileImage;
+
+    public static final String Image="image";
+    public static final String Name="name";
+    public static final String Age="age";
+    public static final String Location="location";
+    public static final String Descr="descr";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+        name = (TextView) findViewById(R.id.name);
+        location = (TextView) findViewById(R.id.location);
+        age = (TextView) findViewById(R.id.age);
+        description = (EditText) findViewById(R.id.description);
+        profileImage = (ImageView) findViewById(R.id.profile_image);
+
+
+        int imageId = getIntent().getIntExtra(Image, -1);
+        profileImage.setImageDrawable(getResources().getDrawable(imageId));
+        name.setText(getIntent().getStringExtra(Name));
+        location.setText(getIntent().getStringExtra(Location));
+        age.setText(getIntent().getStringExtra(Age));
+        description.setText(getIntent().getStringExtra(Descr));
+
+
+        View parent = (View) name.getParent();
+        parent.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+        name.setTextAppearance(this,android.R.style.TextAppearance_DeviceDefault_Large);
+        location.setTextAppearance(this, android.R.style.TextAppearance_DeviceDefault_Medium);
+        location.setTextAppearance(this, android.R.style.TextAppearance_DeviceDefault_Inverse);
+        age.setTextAppearance(this, android.R.style.TextAppearance_DeviceDefault_Inverse);
+        description.setEnabled(false);
+        description.setBackgroundColor(getResources().getColor(android.R.color.white));
+        description.setTextColor(getResources().getColor(android.R.color.black));
+
+
+    }
+}
