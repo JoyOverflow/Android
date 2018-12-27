683d7f5095c2c6dd552e9a9167983144c043b59d
joyoverflow <joyoverflow@sina.com>
2018-12-27 23:42:02 +0800

    创建CircleImage

---
+++ circleimageview/src/main/java/pxgd/hyena/com/circleimageview/CircleImage.java
@@ -0,0 +1,17 @@
+package pxgd.hyena.com.circleimageview;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+public class CircleImage extends AppCompatImageView {
+    public CircleImage(Context context) {
+        super(context);
+    }
+    public CircleImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public CircleImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
