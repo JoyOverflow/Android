d2c136afb4c9bc0c214255d50adc8a17c5044e72
joyoverflow <joyoverflow@sina.com>
2018-12-29 22:28:46 +0800

    添加演示模块

---
+++ mydemo/src/main/java/pxgd/hyena/com/mydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package pxgd.hyena.com.mydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
